47ba0ea79c4ce3ca9f9d08e67393e4d1fb1f8a5e
sravanpeddaraju <ronitnuguru@Ronits-MacBook-Pro.local>
2017-06-05 13:27:48 -0600

    React Framework

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>React Boilerplate Framework. Build your apps on top of this framework</div>
+    );
+  }
+}
